60706f2d082675f55e6b623ad3753e4a4e5dcea0
Andrew Mirsky <andrew@mirsky.net>
2022-11-25 18:25:51 -0500

    cnc : work offset commands
    - G53 (machine space)
    - G54-9 (select coordinate system)
    - G10L2 (set coordinate system offsets)
    
    Signed-off-by: Andrew Mirsky <andrew@mirsky.net

---
+++ klippy/extras/cnc.py
@@ -0,0 +1,33 @@
+# adds support for coordinate system work offsets
+#
+# Copyright (C) 2022-3  Andrew Mirsky <andrew@mirsky.net>
+#
+# This file may be distributed under the terms of the GNU GPLv3 license.
+#
+#
+import pickle, os
+from extras.work_offsets import WorkOffsetsSupport
+from extras.gcode_arcs import ArcSupport
+
+
+class CncSupport:
+
+    def __init__(self, config):
+        self.printer = config.get_printer()
+        wo_config = config.getchoice('work_offsets',
+                                     {'standard': 'standard', 'none': 'none'},
+                                     default='standard')
+        if wo_config == 'standard':
+            self.work_offsets = WorkOffsetsSupport(config)
+        arc_config = config.getboolean('arcs', default=True)
+        gcode_arc_obj = self.printer.lookup_object('gcode_arc', None)
+        if gcode_arc_obj and not arc_config:
+            raise self.printer.config_error(
+                "conflict: section `[gcode_arc]` included, "
+                "but disabled in `[cnc]` section")
+        if arc_config and not gcode_arc_obj:
+            self.arc_commands = ArcSupport(config)
+
+
+def load_config(config):
+    return CncSupport(config)
